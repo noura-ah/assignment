7312d19e4b4c99dcbff6b3df6435d30aa6fd9a9a
Norah Alhumaidan <100047315+noura-ah@users.noreply.github.com>
2022-10-06 22:00:39 +0300

    Delete server directory

--- server/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-
-    validates :email, presence:true,:uniqueness => true, format:{with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message:"must be a vaild email address"}
-    validates :name, presence:true
-    validates :role, presence:true
-end
--- server/db/migrate/20221005034500_add_test_id_to_questions.rb
+++
@@ -1,6 +0,0 @@
-class AddTestIdToQuestions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :questions, :test_id, :integer
-    add_index :questions, :test_id
-  end
-end
